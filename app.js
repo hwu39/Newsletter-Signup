39920a123e7eb02db2b35f4e3caf52b1aeac2541
hwu39 <hwu2@umbc.edu>
2019-05-04 14:24:43 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+//jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us20.api.mailchimp.com/3.0/lists/e4cedea53d",
+    method: "POST",
+    headers: {
+      'Authorization': "hwu39 bfcfaa7ca5329329d7658c5d97216888-us20"
+    },
+    body: jsonData
+  };
+  request(options, function(error, response, body) {
+    console.log(response.statusCode);
+    if (error) {
+      res.sendFIle(__dirname +"/failure.html");
+    }
+    else {
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      }
+      else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+  });
+});
+
+app.post("/success", function(req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000");
+});
+//API key
+// bfcfaa7ca5329329d7658c5d97216888-us20
+
+//Audience // ID:
+//e4cedea53d
